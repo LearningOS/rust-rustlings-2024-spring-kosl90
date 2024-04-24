1db0e72435429dbbb71767074b7ee278cb995826
kosl90 <kos1990l@gmail.com>
2024-04-24 19:56:00 +0800

    fix test8

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -19,6 +19,6 @@ fn main() {
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "pass";
-    println!("cargo:rustc-cfg=CFG=\"{}\"", your_command);
+    let your_command = "rustc-cfg=feature=\"pass\"";
+    println!("cargo:{}", your_command);
 }
